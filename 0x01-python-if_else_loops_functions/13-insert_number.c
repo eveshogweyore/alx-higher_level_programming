75436ae879b3294ca674a09f33bf484deb387610
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-03 10:58:38 -0800

    Task 13 REDONE (logic reveamped to check for the number in the next node as opposed to the former logic that checks for the number in the current node)

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -16,10 +16,10 @@ listint_t *insert_node(listint_t **head, int number)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = number;
-	if (*head == NULL)
+	if (*head == NULL || (*head)->n > number)
 	{
+		new_node->next = *head;
 		*head = new_node;
-		new_node->next = NULL;
 		return (new_node);
 	}
 
