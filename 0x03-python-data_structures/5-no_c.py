bd65d862634454530cad0b7b4e1d053b4dde03d2
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-08 06:14:00 -0800

    Task 5 REDONE

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,6 +5,7 @@ def no_c(my_string):
     while count <= len(list_string) - 1:
         if list_string[count] in 'cC':
             list_string.pop(count)
+            continue
         count += 1
     new_string = ''
     for i in list_string:
