74115b8c0cbb80755fa02d40fcd71ef5f5a297ac
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-08 07:35:33 -0800

    Task 6 REDONE

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for inner_list in matrix:
-        for i, j in enumerate(l):
+        for i, j in enumerate(inner_list):
             if i == len(inner_list) - 1:
                 print('{:d}'.format(j), end='')
             else:
