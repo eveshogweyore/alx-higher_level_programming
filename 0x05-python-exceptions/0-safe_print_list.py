93afb32afe60f42cccd97f19506b57cccdb6ecfe
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-22 04:29:20 -0800

    Conform code to pycodestyle standard.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     while i < x:
         try:
             print("{}".format(my_list[i]), end="")
-        except:
+        except IndexError:
             break
         i += 1
     print()
