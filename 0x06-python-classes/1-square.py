5649f4e6d40e4231fba2bb384b390fea89ef723d
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-23 05:39:34 -0800

    Task 1 DONE

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Module to create all functionalities for Square class."""
+
+
+class Square:
+    """A square class."""
+
+    def __init__(self, size):
+        """Square __init__ method.
+
+        size : int
+            Store the size of a Square object.
+        """
+        self.__size = size
