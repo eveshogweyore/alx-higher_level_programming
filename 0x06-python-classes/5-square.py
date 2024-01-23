62a7778f5cce20b15fcaaf9cc28c47c26876b211
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-23 07:09:33 -0800

    Task 5 DONE

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Module to create all functionalities for Square class."""
+
+
+class Square:
+    """A square class."""
+
+    def __init__(self, size=0):
+        """Square __init__ method.
+
+        size : int
+            Store the size of a Square object.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+            Parameter
+            ---------
+            value : int
+                New value to be set as size of Square object.
+            Returns
+            -------
+            int
+                size of Square object
+
+            Setter method checks for the following errors:
+            (1) TypeError: Checks if value is an integer.
+            (2) ValueError: Checks is value is less than zero.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculates the square area of square object.
+        Returns
+        -------
+        int
+            Square area of square object.
+        """
+
+        return self.__size**2
+
+    def my_print(self):
+        """Prints an actual square."""
+        if (self.__size == 0):
+            print()
+
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("{}".format("#"), end="")
+            print()
