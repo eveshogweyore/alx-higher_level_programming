7842902bbc5583e85f8fd88ae71aef5b39ec8e10
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-23 05:59:38 -0800

    Task 3 DONE

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Module to create all functionalities for Square class."""
+
+
+class Square:
+    """A square class."""
+
+    def __init__(self, size=0):
+        """Square __init__ method.
+
+        size : int
+            Store the size of a Square object.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculates the square area of square object.
+        Returns
+        -------
+        int
+            Square area of square object.
+        """
+
+        return self.__size**2
