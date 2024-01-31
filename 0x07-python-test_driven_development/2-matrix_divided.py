b7f9b9e82563ab41234733c5d8d3d63b521e8cd9
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-31 01:27:24 -0800

    Add logic and doctest to check for empty or incomplete arguments passed and for float(inf) passed as an argument.

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module to handle logic associated with dividing a matrix."""
+
 
 def matrix_divided(matrix, div):
     """Divides all elements of a matrix.
@@ -17,14 +19,17 @@ def matrix_divided(matrix, div):
     """
     new_list = []
     idx = 0
+    err = "matrix must be a matrix (list of lists) of integers/floats"
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
     elif not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
+    elif div == float("inf") or div == float("-inf"):
+        raise TypeError("div must be a number")
 
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(err)
 
     length = len(matrix[0])
 
@@ -34,7 +39,7 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
         for j in inner_list:
             if not isinstance(j, int) and not isinstance(j, float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError(err)
             new_list[idx].append(round(j/div, 2))
 
         idx += 1
