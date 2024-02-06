06963854cb8a1144fda3fc2f4ce37c18835b0d06
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-02-06 09:01:24 -0800

    Add logic to BaseGeometry class to raise an Exception.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A module to improve on the BaseGeometry class. """
+
+
+class BaseGeometry:
+    """ A BaseGeometry class."""
+
+    def area(self):
+        """ A method that raises an exception. """
+        raise Exception("area() is not implemented")
