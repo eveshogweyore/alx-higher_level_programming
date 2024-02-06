9cc52a0e00b66e17dc166a4c7c2f4c18627bc213
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-02-06 08:19:40 -0800

    Logic to check if object is a subclass of another class.

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" A module to check if an object inherits from a class. """
+
+
+def inherits_from(obj, a_class):
+    """ A function that checks if an object inherits from a class
+
+    Parameters
+    ----------
+    obj : object
+        The object to be checked for.
+    a_class : objectType
+        The class associated with the object.
+
+    Returns
+    -------
+        On success, True. Othewise, False.
+    """
+    return issubclass(type(obj), a_class)
