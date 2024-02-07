73ac30912c4336ab9b7f9fa314d423c90f22b9c8
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-02-07 02:43:45 -0800

    Remove definition of BaseClass in module.

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,20 +2,7 @@
 """ A Rectangle class module. """
 
 
-class BaseGeometry:
-    """ A BaseGeometry class."""
-
-    def area(self):
-        """ A method that raises an exception. """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Checks if the the values passed are valid. """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ A Rectangle class that inherits from BaseGeometry class"""
