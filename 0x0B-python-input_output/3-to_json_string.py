9f279d99f56531fdf6f0f5fb452f98b617658f98
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-02-08 05:25:24 -0800

    Conform codebase to pycodestyle.

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """ A module returns JSON representation of an object. """
 import json
 
+
 def to_json_string(my_obj):
     """ Returns object's JSON representation. """
     return json.dumps(my_obj)
