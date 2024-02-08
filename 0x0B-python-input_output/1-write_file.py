44ea3a690dd9cb5dc02d9bc19d335028ab5a0257
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-02-08 05:05:16 -0800

    Logic to write a string to a text file.

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A module to write a string to a text file. """
+
+
+def write_file(filename="", text=""):
+    """ Write a string to a text file and return the number of 
+    character
+    """
+    with open(filename, "w", encoding="utf-8") as file_obj:
+        file_obj.write(text)
+        return len(text)
