9f48a9fdfc5a5d58b81dc2cf7dc1528920abbd39
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-02-09 04:24:44 -0800

    Conform codebase to pycodestyle standard.

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,19 +3,23 @@
     A module that adds all arguments to a Python list and
     save them to a file
 """
-import pathlib, sys, json
+import json
+import sys
+import pathlib
 
 save = __import__("5-save_to_json_file").save_to_json_file
 load = __import__("6-load_from_json_file").load_from_json_file
 
-if pathlib.Path("add_item.json").exists():
-    items = load("add_item.json")
+json_file = "add_item.json"
+
+if pathlib.Path(json_file).exists():
+    items = load(json_file)
 else:
-    with open("add_item.json", "w") as file_obj:
+    with open(json_file, "w") as file_obj:
         items = []
 
 for i in range(len(sys.argv)):
     if i != 0:
         items.append(sys.argv[i])
 
-save(items, "add_item.json")
+save(items, json_file)
