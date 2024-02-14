8f745195ef65c44c80ca5e662c20570c9d9154ee
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-02-14 04:18:57 -0800

    Add getter and setter method for the size attribute in the Square class.

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,3 +12,18 @@ class Square(Rectangle):
     def __str__(self):
         """ Human-readable representation of Square class. """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    @property
+    def size(self):
+        """ value: the new set width and height for square. """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value <= 0:
+                raise ValueError("width must be > 0")
+            self.width = value
+            self.height = value
+        else:
+            raise TypeError("width must be an integer")
