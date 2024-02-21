e318ae086e4ba5f8c60c0805506d991f4818e50a
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-02-21 08:59:05 -0800

    Handle edge case for when only 1 argument is passed.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,10 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("0 arguments.")
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
+        if len(sys.argv) == 2:
+            print("{} argument:".format(len(sys.argv) - 1))
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
         for i in range(len(sys.argv)):
             if i != 0:
                 print("{}: {}".format(i, sys.argv[i]))
