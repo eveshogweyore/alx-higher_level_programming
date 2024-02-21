6151794c9b8b44b6edbcb3870aecc5fd503d1ff2
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-02-21 08:51:34 -0800

    Add logic to print the number of and the list of its argument.

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv)):
+            if i != 0:
+                print("{}: {}".format(i, sys.argv[i]))
