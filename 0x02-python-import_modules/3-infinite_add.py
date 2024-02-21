8db5f7902cca5f472b2f5648dabc23a3f79d3496
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-02-21 09:09:19 -0800

    Add logic to print the result of the addition of all arguments.

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    sum = 0
+
+    if len(sys.argv) == 1:
+        print(sum)
+    else:
+        for i in range(len(sys.argv)):
+            if i > 0:
+                sum += int(sys.argv[i])
+        print(sum)
