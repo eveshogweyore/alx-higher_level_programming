499036fe04cbb59e8e26a43e49dd80814f842db6
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-04-08 18:11:00 +0100

    Add script to print the size of a square.

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const { argv } = require('node:process');
+
+if (argv[2] === undefined) {
+  console.log('Missing size');
+}
+
+const size = parseInt(argv[2]);
+
+for (let i = 0; i < size; i++) {
+  for (let j = 0; j < size; j++) {
+    process.stdout.write('X');
+  }
+  console.log('');
+}
