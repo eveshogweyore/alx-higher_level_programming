3ad875369e45e6c67c69cc067fe2e5c9d58cd19f
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-04-13 14:03:55 +0100

    Refactor: Use argv variable to collect all command line arguments.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,16 +5,18 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
 
+    argv = sys.argv
+
     db = MySQLdb.connect(
         host='localhost',
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
     )
 
     cur = db.cursor()
-    cur.execute(f"SELECT * FROM {sys.argv[3]}.states ORDER BY states.id ASC")
+    cur.execute(f"SELECT * FROM {argv[3]}.states ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
