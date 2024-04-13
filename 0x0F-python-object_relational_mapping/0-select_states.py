0543c42a71f8425fdf6cc1413f32e8295100be21
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-04-13 11:31:14 +0100

    Bug fix: Adjust the insertion of command-line arguments to source code.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
     argv = sys.argv
 
     db = sql.connect(
-        host='localhost:3306',
+        host='localhost',
+		port=3306,
         user=argv[1],
         passwd=argv[2],
         db=argv[3]
