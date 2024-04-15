ba9bf0ee196c66d5dc23bba077b258ef25f04623
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-04-15 19:24:54 +0100

    Make sure code does not execute when imported.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """List all state objects from database."""
 
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
 
-db_url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+    db_url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
 
-engine = create_engine(db_url)
+    engine = create_engine(db_url)
 
-Session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
 
-session = Session()
+    session = Session()
 
-states = session.query(State).all()
+    states = session.query(State).all()
 
-for state in states:
-    print(f"{state.id}: {state.name}")
+    for state in states:
+        print(f"{state.id}: {state.name}")
 
-session.close()
+    session.close()
