c35b48ee26a56b8de7bdb54ddb952ec337f17acd
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-04-16 14:17:05 +0100

    Add script to add the State object 'Louisiana' to the database.

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Add the State object 'Louisiana' to the database."""
+
+if __name__ == "__main__":
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    db_url = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
+
+    engine = create_engine(db_url)
+
+    Session = sessionmaker(bind=engine)
+
+    with Session() as session:
+        new_state = State(name='Louisiana')
+        session.add(new_state)
+        session.commit()
