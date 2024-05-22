9cfd3d8d7c20a665be8d735339afb8db96aff1f1
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-05-22 13:57:32 +0100

    Add script that writes a string to a file.

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const process = require('process')
+const fs = require('node:fs')
+
+const file = process.argv[2]
+const content = process.argv[3]
+
+fs.writeFile(file, content, err => {
+  if (err) {
+    console.error(err);
+  }
+});
